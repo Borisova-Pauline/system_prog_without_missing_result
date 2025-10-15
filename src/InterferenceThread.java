d6637277b18cdce683e7fd2e9abce2238567db8c
Pauline <some@mail>
2025-10-15 09:56:16 +0300

    done

--- src/InterferenceThread.java
+++ src/InterferenceThread.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class InterferenceThread {
+public class InterferenceThread extends Thread {
+    private final InterferenceExample checker;
+    private static int i;
+
+    public static final ReentrantLock lock=new ReentrantLock();
+
+
+    InterferenceThread(InterferenceExample checker) {
+        this.checker = checker;
+    }
+
+    private void increment(){
+        i++;
+    }
+
+    int getI() {
+        return i;
+    }
+
+    public void run() {
+        while (!checker.stop()) {
+            lock.lock();
+            increment();
+            lock.unlock();
+        }
+    }
 }
